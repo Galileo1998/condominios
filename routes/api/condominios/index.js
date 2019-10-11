da468a53644b1c58fc4f1e7a0c4aaabd401a7e0d
Galileo <galileoga63@gmail.com>
2019-10-11 00:56:48 -0600

    Tercer Commit
    Post

--- routes/api/condominios/index.js
+++ routes/api/condominios/index.js
@@ -16,4 +16,36 @@ router.get('/all', function(req, res){
     res.json(conCollection);
   }); // get /all
 
+router.post('/new', function(req, res){
+    conCollection = fileModel.getCondominios();
+    var newCondominios = Object.assign(
+       {},
+       req.body,
+       {
+           "nombre": req.body.nombre,
+           "apartament": parseInt(req.body.apartament),
+           "cuotaMensual": parseFloat(req.body.cuotaMensual)
+       }
+    );
+    var condominiosExists = conCollection.find(
+      function(o, i){
+        return o.codigo === newCondominios.codigo;
+      }
+    )
+    if( ! condominiosExists ){
+      conCollection.push(newCondominios);
+      fileModel.setCondominios(
+         conCollection,
+         function(err, savedSuccesfully){
+           if(err){
+             res.status(400).json({ "error": "No se pudo ingresar objeto" });
+           } else {
+             res.json(newCondominios);  // req.body ===  $_POST[]
+           }
+         }
+       );
+    } else {
+      res.status(400).json({"error":"No se pudo ingresar objeto"});
+    }
+ }); // post /new
 module.exports = router;
